8f9d976e7e9a9f7806f05649612ee0575721590a
IcePlayer <nathanchevalet@orange.fr>
2022-07-24 12:28:20 +0200

    import auth files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,53 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+
+// External Library
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+// Service
 import { AppService } from './app.service';
 
+// Entity
+import { User } from './users/user.entity';
+// import { RefreshToken } from './token/refresh-token.entity';
+
+// Controller
+import { AppController } from './app.controller';
+
+// Module
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        ACCESS_TOKEN_SECRET: Joi.string().required(),
+        REFRESH_TOKEN_SECRET: Joi.string().required(),
+        DB_TYPE: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        IS_DEV: Joi.boolean().required(),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [User],
+      synchronize: Boolean(process.env.IS_DEV) || false,
+    }),
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
