a27eeaa729b5725662b55b793828b4f72dc7c85b
IcePlayer <nathanchevalet@orange.fr>
2022-07-25 10:57:27 +0200

    revamp file

--- src/token/dto/auth-payload.dto.ts
+++ src/token/dto/auth-payload.dto.ts
@@ -1,10 +1,7 @@
-import { User } from '../../users/user.entity';
-
 export interface AuthPayloadDto {
-  user: User;
-  payload: {
-    type: string;
-    token: string;
-    refresh_token?: string;
-  };
+  id: number;
+  email: string;
+  username: string;
+  accessToken: string;
+  refresh_token?: string;
 }
