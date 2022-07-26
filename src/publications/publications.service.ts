b6ac263b5fc3c9395e5f0a39a2ce29721c65f83a
IcePlayer <nathanchevalet@orange.fr>
2022-07-26 10:07:46 +0200

    add gestion of delete/update/get

--- src/publications/publications.service.ts
+++ src/publications/publications.service.ts
@@ -1,22 +1,25 @@
 import {
   Injectable,
-  // NotFoundException,
+  NotFoundException,
   UnprocessableEntityException,
 } from '@nestjs/common';
 import { Publication } from './publication.entity';
+import { User } from '../users/user.entity';
 import { CreatePublicationDto, UpdatePublicationDto } from './dto';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class PublicationsService {
   constructor(
     @InjectRepository(Publication)
-    private postsRepository: Repository<Publication>,
+    private publicationsRepository: Repository<Publication>,
+    private usersService: UsersService,
   ) {}
 
   async findAll(): Promise<Publication[]> {
-    return this.postsRepository.find({
+    return this.publicationsRepository.find({
       select: {
         id: true,
         createdAt: true,
@@ -30,35 +33,56 @@ export class PublicationsService {
   }
 
   async findOne(id: number): Promise<Publication> {
-    return this.postsRepository.findOneOrFail({
-      where: {
-        id,
-      },
-      select: {
-        id: true,
-        createdAt: true,
-        content: true,
-        media: true,
-        user: {
-          username: true,
+    try {
+      return await this.publicationsRepository.findOneOrFail({
+        where: {
+          id,
         },
-      },
-    });
+        relations: {
+          user: true,
+        },
+        select: {
+          id: true,
+          createdAt: true,
+          content: true,
+          media: true,
+          user: {
+            username: true,
+          },
+        },
+      });
+    } catch (err) {
+      throw new NotFoundException();
+    }
   }
 
-  // async create(createUserDto: CreatePostDto) {
-  //   const post = new Publication();
-  //   post.content = createUserDto.username;
-  //   post.media = createUserDto.email;
-  //   post.user = createUserDto.password;
-  //
-  //   try {
-  //     await this.findByEmail(createUserDto.email);
-  //   } catch (err) {
-  //     await this.usersRepository.save(user);
-  //     delete user.password;
-  //     return user;
-  //   }
-  //   throw new UnprocessableEntityException('Email already in use');
-  // }
+  async create(createPublicationDto: CreatePublicationDto, user: User) {
+    const publication = new Publication();
+    const userInfos = await this.usersService.findById(user.id);
+
+    publication.content = createPublicationDto.content;
+    // publication.media = createPublicationDto.media;
+    publication.user = userInfos;
+    try {
+      await this.publicationsRepository.save(publication);
+    } catch {
+      throw new UnprocessableEntityException('An error has occurred');
+    }
+  }
+
+  async update(updatePublicationDto: UpdatePublicationDto, id: number) {
+    try {
+      const publication = await this.publicationsRepository.findOneBy({ id });
+
+      publication.content = updatePublicationDto.content;
+      // publication.media = updatePublicationDto.media;
+      return this.publicationsRepository.save(publication);
+    } catch (err) {
+      throw new NotFoundException("This publication don't exist.");
+    }
+  }
+
+  deleteOne(id: number) {
+    return this.publicationsRepository.delete(id);
+  }
 }
