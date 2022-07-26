b952c1f7d7efbb233dd6335c3df372659358e404
IcePlayer <nathanchevalet@orange.fr>
2022-07-26 10:07:22 +0200

    import user module

--- src/publications/publications.module.ts
+++ src/publications/publications.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 
 // Service
 import { PublicationsService } from './publications.service';
+import { UsersModule } from '../users/users.module';
 import { PublicationsController } from './publications.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Publication } from './publication.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Publication])],
+  imports: [TypeOrmModule.forFeature([Publication]), UsersModule],
   providers: [PublicationsService],
   controllers: [PublicationsController],
   exports: [PublicationsService],
