f95e63e023b9adb94bbfb7066e7184b15c8c0d1e
IcePlayer <nathanchevalet@orange.fr>
2022-07-27 23:28:33 +0200

    remove config for aws sdk

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+// import { config } from 'aws-sdk';
 
 async function bootstrap() {
+  // config.update({
+  //   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  //   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  //   region: process.env.AWS_REGION,
+  // });
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
