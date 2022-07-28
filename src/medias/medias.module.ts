edde5530e51fc5e9607ad48978038fde7e333df5
IcePlayer <nathanchevalet@orange.fr>
2022-07-28 09:51:42 +0200

    import typeORM & user module

--- src/medias/medias.module.ts
+++ src/medias/medias.module.ts
@@ -4,8 +4,12 @@ import { Module } from '@nestjs/common';
 import { MediasService } from './medias.service';
 import { FilesService } from './files.service';
 import { MediasController } from './medias.controller';
+import { UsersModule } from '../users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Media } from './media.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Media]), UsersModule],
   providers: [MediasService, FilesService],
   controllers: [MediasController],
   exports: [MediasService],
