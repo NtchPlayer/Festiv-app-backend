ab954b9a25ce22718ce1a40c5ab57d2ba0ac4ce1
IcePlayer <nathanchevalet@orange.fr>
2022-07-28 09:51:09 +0200

    add media entity gestion

--- src/medias/medias.service.ts
+++ src/medias/medias.service.ts
@@ -1,21 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from "@nestjs/common";
+import { Express } from 'express';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
+//Service
 import { FilesService } from './files.service';
+import { UsersService } from '../users/users.service';
+
+// Entity
+import { Media } from './media.entity';
+import { User } from '../users/user.entity';
 
 @Injectable()
 export class MediasService {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(
+    @InjectRepository(Media)
+    private mediasRepository: Repository<Media>,
+    private readonly filesService: FilesService,
+    private readonly usersService: UsersService,
+  ) {}
 
-  async addAvatar(imageBuffer: Buffer, fileName: string, contentType: string) {
-    return this.filesService.uploadFile(
-      imageBuffer,
-      fileName,
+  async addUserAvatar(file: Express.Multer.File, user: User) {
+    const media = new Media();
+    const userInfos = await this.usersService.findById(user.id);
+
+    const upload = await this.filesService.uploadFile(
+      file.buffer,
+      file.originalname,
       'avatars',
-      contentType,
+      file.mimetype,
     );
+
+    media.url = upload.Location;
+    media.key = upload.Key;
+    media.user = userInfos;
+
+    try {
+      await this.mediasRepository.save(media);
+    } catch {
+      throw new UnprocessableEntityException('An error has occurred');
+    }
   }
 
-  async addPostImage(
+  async addPublicationMedia(
     imageBuffer: Buffer,
     fileName: string,
     contentType: string,
