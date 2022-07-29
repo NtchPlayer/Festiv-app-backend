2882a5c54cf6ab7437685bd5eab6c79c1465cf80
IcePlayer <nathanchevalet@orange.fr>
2022-07-29 21:55:00 +0200

    remove console log

--- src/medias/file-helpers.ts
+++ src/medias/file-helpers.ts
@@ -1,5 +1,4 @@
 export const imageFileFilter = (req, file, callback) => {
-  console.log(file);
   if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
     req.fileValidationError = 'Only image file are allowed!';
     return callback(null, false);
