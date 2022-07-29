924566014d5028d96e325886bd9480fa72b3abf0
IcePlayer <nathanchevalet@orange.fr>
2022-07-29 21:57:07 +0200

    remove console log

--- src/publications/publications.controller.ts
+++ src/publications/publications.controller.ts
@@ -25,7 +25,6 @@ export class PublicationsController {
 
   @Get('/')
   getAll(@Query('username') username?: string) {
-    console.log(username);
     return this.publicationsService.findAll(username);
   }
 
