647f17819a7099a77678349d7742640bcfce2b59
IcePlayer <nathanchevalet@orange.fr>
2022-07-30 17:45:52 +0200

    revamp file

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,11 +1,34 @@
-import { Matches } from 'class-validator';
+import {
+  IsArray,
+  IsEmail,
+  IsOptional,
+  IsString,
+  Matches,
+  // MaxDate,
+  MinLength,
+} from 'class-validator';
 
 export class UpdateUserDto {
+  @IsOptional()
+  @MinLength(2)
   username?: string;
+
+  @IsOptional()
+  @IsEmail()
   email?: string;
 
-  @Matches(/^\d{4}(-)(((0)[0-9])|((1)[0-2]))(-)([0-2][0-9]|(3)[0-1])$/i, {
+  @IsOptional()
+  @Matches(/^\d{4}(-)(((0)\d)|((1)[0-2]))(-)([0-2]\d|(3)[0-1])$/i, {
     message: 'Birthday must be formatted as yyyy-mm-dd',
   })
+  //@MaxDate(new Date('2009-07-30'))
   birthday?: string;
+
+  @IsOptional()
+  @IsString()
+  biography?: string;
+
+  @IsOptional()
+  @IsArray()
+  tags: [{ content: string; id?: number }];
 }
