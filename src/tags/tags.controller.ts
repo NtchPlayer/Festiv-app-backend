34532383017636b17d881fb8944a8caa4a150da4
IcePlayer <nathanchevalet@orange.fr>
2022-07-30 17:44:27 +0200

    file revamp

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,42 +1,27 @@
 import {
   Controller,
-  Get,
   Post,
   Body,
   Param,
   ParseIntPipe,
   Delete,
-  Put,
+  Request,
 } from '@nestjs/common';
 
 import { TagsService } from './tags.service';
-import { UpdateTagDto } from './dto';
+import { CreateTagDto } from './dto';
 
 @Controller('tags')
 export class TagsController {
   constructor(private readonly tagsService: TagsService) {}
 
-  // @Get('/')
-  // getAll(@Query('username') username?: string) {
-  //   console.log(username);
-  //   return this.publicationsService.findAll(username);
-  // }
-  //
-  // @Get('/:id')
-  // getOne(@Param('id', ParseIntPipe) id: number) {
-  //   return this.publicationsService.findOne(id);
-  // }
-
-  @Put('/:id')
-  async update(
-    @Body() updateTagDto: UpdateTagDto,
-    @Param('id', ParseIntPipe) id: number,
-  ) {
-    return this.tagsService.update(updateTagDto, id);
+  @Post('add')
+  async create(@Request() req, @Body() createTagDto: CreateTagDto) {
+    return this.tagsService.add(createTagDto, req.user.userId);
   }
 
-  // @Delete('/:id')
-  // async delete(@Param('id', ParseIntPipe) id: number, @Request() req) {
-  //   return this.publicationsService.deleteOne(id, req.user.userId);
-  // }
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id: number, @Request() req) {
+    return this.tagsService.deleteOne(id, req.user.userId);
+  }
 }
