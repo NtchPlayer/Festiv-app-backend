43d40846dab8d0d0d0d8058581539b8dde6f33b8
IcePlayer <nathanchevalet@orange.fr>
2022-07-30 21:15:28 +0200

    import TagsModule

--- src/publications/publications.module.ts
+++ src/publications/publications.module.ts
@@ -4,12 +4,18 @@ import { Module } from '@nestjs/common';
 import { PublicationsService } from './publications.service';
 import { UsersModule } from '../users/users.module';
 import { MediasModule } from '../medias/medias.module';
+import { TagsModule } from '../tags/tags.module';
 import { PublicationsController } from './publications.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Publication } from './publication.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Publication]), UsersModule, MediasModule],
+  imports: [
+    TypeOrmModule.forFeature([Publication]),
+    UsersModule,
+    MediasModule,
+    TagsModule,
+  ],
   providers: [PublicationsService],
   controllers: [PublicationsController],
   exports: [PublicationsService],
