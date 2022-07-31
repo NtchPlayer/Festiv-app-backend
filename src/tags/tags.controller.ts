bdca2d9bca6fc0757fd2472c2c3cdf8885ff594d
IcePlayer <nathanchevalet@orange.fr>
2022-07-31 09:37:59 +0200

    comment add endpoint

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -15,10 +15,10 @@ import { CreateTagDto } from './dto';
 export class TagsController {
   constructor(private readonly tagsService: TagsService) {}
 
-  @Post('add')
-  async create(@Request() req, @Body() createTagDto: CreateTagDto) {
-    return this.tagsService.add(createTagDto, req.user.userId);
-  }
+  // @Post('add')
+  // async create(@Request() req, @Body() createTagDto: CreateTagDto) {
+  //   return this.tagsService.add(createTagDto, req.user.userId);
+  // }
 
   @Delete(':id')
   async delete(@Param('id', ParseIntPipe) id: number, @Request() req) {
