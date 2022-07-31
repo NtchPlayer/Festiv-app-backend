9d28b0596e8d5b9f4a645cdbed024f7f7a237ad5
IcePlayer <nathanchevalet@orange.fr>
2022-07-31 09:38:20 +0200

    remove import of usersModule

--- src/tags/tags.module.ts
+++ src/tags/tags.module.ts
@@ -1,14 +1,13 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 
 // Service
 import { TagsService } from './tags.service';
 import { TagsController } from './tags.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tag } from './tag.entity';
-import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tag]), UsersModule],
+  imports: [TypeOrmModule.forFeature([Tag])],
   providers: [TagsService],
   controllers: [TagsController],
   exports: [TagsService],
