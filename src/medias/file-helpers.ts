0969f1871c3d6d77bb3dc7bcfcbad77ed15b74f9
IcePlayer <nathanchevalet@orange.fr>
2022-08-05 22:22:42 +0200

    remove unused file

--- src/medias/file-helpers.ts
+++
@@ -1,7 +0,0 @@
-export const imageFileFilter = (req, file, callback) => {
-  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-    req.fileValidationError = 'Only image file are allowed!';
-    return callback(null, false);
-  }
-  callback(null, true);
-};
