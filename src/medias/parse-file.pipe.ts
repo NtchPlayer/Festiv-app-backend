c7c2209b0139dc50a1f573bf409616b10be312f8
IcePlayer <nathanchevalet@orange.fr>
2022-08-05 22:22:47 +0200

    init file

---
+++ src/medias/parse-file.pipe.ts
@@ -0,0 +1,35 @@
+import { Injectable, PipeTransform, BadRequestException } from '@nestjs/common';
+
+@Injectable()
+export class ParseFile implements PipeTransform {
+  transform(
+    files: Express.Multer.File[],
+  ): Express.Multer.File | Express.Multer.File[] {
+    if (
+      !files.every((file) =>
+        file.originalname.match(/\.(jpg|jpeg|png|gif|webp|mp4|mov|m4v|avi)$/),
+      )
+    ) {
+      throw new BadRequestException(
+        'Validation failed (type mine not authorized)',
+      );
+    }
+    const fileType = files[0].mimetype.split('/')[0];
+    if (!files.every((file) => file.mimetype.split('/')[0] === fileType)) {
+      throw new BadRequestException('Validation failed (send video and image)');
+    }
+    if (files.length > 2) {
+      throw new BadRequestException(
+        'Validation failed (more than 2 files send)',
+      );
+    }
+    if (
+      files.every(
+        (file) => parseFloat((file.size / 1000000).toPrecision(3)) > 10, // MegaOctet
+      )
+    ) {
+      throw new BadRequestException('Validation failed (files size exceed)');
+    }
+    return files;
+  }
+}
