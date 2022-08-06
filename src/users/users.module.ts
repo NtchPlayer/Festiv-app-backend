873b8a03845405f94deac97ca49a92e8d7d9bd31
IcePlayer <nathanchevalet@orange.fr>
2022-08-06 02:10:03 +0200

    add import of file service and media entity for repository

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -7,7 +7,9 @@ import { TokenService } from '../token/token.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { Media } from '../medias/media.entity';
 import { TagsModule } from '../tags/tags.module';
+import { FilesService } from '../medias/files.service';
 // import { RefreshToken } from '../token/refresh-token.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -16,6 +18,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
   imports: [
     TypeOrmModule.forFeature([
       User,
+      Media,
       // RefreshToken
     ]),
     JwtModule.registerAsync({
@@ -27,7 +30,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     }),
     TagsModule,
   ],
-  providers: [UsersService, TokenService],
+  providers: [UsersService, TokenService, FilesService],
   controllers: [UsersController],
   exports: [UsersService],
 })
