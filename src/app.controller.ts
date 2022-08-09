8ab992fee54679656b9f126925a23de9686c8ac6
IcePlayer <nathanchevalet@orange.fr>
2022-08-09 16:50:42 +0200

    add publication decorator to welcome message

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { Public } from './decorators/public.decorator';
 
 @Controller()
 export class AppController {
   @Get()
+  @Public()
   welcome() {
     return "Welcome to API of Festiv'App project.";
   }
