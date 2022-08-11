76fd3215c286f02aaa028b76d15e7378a2090ec8
IcePlayer <nathanchevalet@orange.fr>
2022-08-11 13:06:27 +0200

    init file

---
+++ src/pipes/file-avatar.pipe.ts
@@ -0,0 +1,16 @@
+import { Injectable, PipeTransform, BadRequestException } from '@nestjs/common';
+
+@Injectable()
+export class FilesPublicationPipe implements PipeTransform {
+  transform(file: Express.Multer.File): Express.Multer.File {
+    if (!file.originalname.toLowerCase().match(/\.(jpg|jpeg|png|webp)$/)) {
+      throw new BadRequestException(
+        'Validation failed (type mine not authorized)',
+      );
+    }
+    if (parseFloat((file.size / 1000000).toPrecision(3)) > 20) {
+      throw new BadRequestException('Validation failed (file size exceed)');
+    }
+    return file;
+  }
+}
