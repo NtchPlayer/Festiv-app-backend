ef78835443e9e448d7b13242ac7ea9c0bc8414f1
IcePlayer <nathanchevalet@orange.fr>
2022-08-11 11:09:51 +0200

    enabled synchronize

--- src/app.module.ts
+++ src/app.module.ts
@@ -51,7 +51,7 @@ import { TagsModule } from './tags/tags.module';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       entities: [User, Publication, Media, Tag],
-      synchronize: Boolean(process.env.IS_DEV) || false,
+      synchronize: true,
     }),
     AuthModule,
     UsersModule,
